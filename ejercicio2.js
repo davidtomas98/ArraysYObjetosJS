64b7140c92eb663b4f05094d2ab0af680c743248
davidtomas98 <aldrove98@gmail.com>
2023-06-28 15:24:00 +0200

    Create ejercicio2.js

---
+++ ejercicio2.js
@@ -0,0 +1,7 @@
+const planetas = ["Mercurio", "Venus", "Tierra", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno"];
+
+function listarPlanetas() {
+  for (let i = 0; i < planetas.length; i++) {
+    console.log(planetas[i]);
+  }
+}
